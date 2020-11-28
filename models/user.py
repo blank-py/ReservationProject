af5cdd277db093c3957765eb853f00f263c6772f
blankdev <jesse.malinen1@edu.turkuamk.fi>
2020-11-28 20:50:36 +0200

    added partial user class

---
+++ models/user.py
@@ -0,0 +1,8 @@
+# File Name: user.py
+# Original Author: Jesse Malinen/blank-py
+# Description: User model
+
+# imports
+from extensions import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
