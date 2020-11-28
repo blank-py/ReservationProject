ae663bddb7b3d27f92e70125de93bc1325aee04e
blankdev <jesse.malinen1@edu.turkuamk.fi>
2020-11-28 21:01:59 +0200

    user table partially done

--- models/user.py
+++ models/user.py
@@ -6,3 +6,20 @@
 from extensions import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable = False, unique=True)
+    email = db.Column(db.String(200), nullable = False, unique = True)
+    password = db.Column(db.String(200))
+    
+    reservations = db.relationship('Reservation', backref='users')
+    
+    @classmethod
+    def get_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+    
+    @classmethod
+    def get_by_email(cls, email):
+        return cls.query.filter_by(email=email).first()
